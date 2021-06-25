475ca0e2eeee5483f2cc579d155aed122c5a13e6
taikihamamoto <taiki200316@icloud.com>
2021-06-25 18:13:41 +0900

    firstcommit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+    <title>HOME</title>
+</head>
+<body>
+    
+</body>
+</html>
